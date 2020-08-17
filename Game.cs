0f5b2ec99b71242e9e95c12078d8180fc6ba12bc
EthanJones <ethanjones800@yahoo.com>
2020-08-17 16:27:17 -0500

    Added player stats. Printed player health.

--- Game.cs
+++ Game.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HelloWorld
 {
@@ -8,6 +6,17 @@ namespace HelloWorld
     {
         public void Run()
         {
+            float health = 100.0f;
+            float healthRegen = 20;
+            string name = Console.ReadLine();
+            bool maxleverlReached = false;
+            int maxlevel = 100;
+            int level = 1;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.Write(name + " gained " + healthRegen + "Hp");
+            
 
         }
     }
